81783423a011ad554fbba26fa11dac5227fd3c9d
qiweiii <yangqiwei97@gmail.com>
2021-12-26 16:42:12 +0800

    add chap 11

---
+++ small-examples/src/chap11/App1.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let radius = 1;
+  $: area = 2 * Math.PI * radius ** 2;
+</script>
+
+{@debug radius, area}
+<main>
+  <label>
+    Circle Radius:
+    <input type="number" bind:value={radius} />
+  </label>
+  Circle Area: {area}
+  (Need to open devtool to take effect)
+</main>
--- small-examples/src/chap11/index.svelte
+++ small-examples/src/chap11/index.svelte
@@ -1,10 +1,25 @@
 <script>
+  import App1 from './App1.svelte';
 </script>
 
 <div class="chap">
   <h1>Chap 11: Debugging</h1>
 
   <div class="example">
-    <h2>xx example: <code>xx</code></h2>
+    <h2>Debug example 1: <code>@debug</code></h2>
+    <App1 />
+  </div>
+
+  <div class="example">
+    <h2>Debug example 2: <code>reactive statements</code></h2>
+    <div>Basically, print stuff out on state change...</div>
+    <code>$: console.log('count on date', date, 'is', count);</code>
+  </div>
+
+  <div class="example">
+    <h2>Debug example 3: <code>Svelte Devtools</code></h2>
+    <a href="https://github.com/RedHatter/svelte-devtools"
+      >github.com/RedHatter/svelte-devtools</a
+    >/>
   </div>
 </div>
